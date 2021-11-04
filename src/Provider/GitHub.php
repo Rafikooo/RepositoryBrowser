ce3f239281a5f977580c18649dad7e3d7917146b
Rafikooo <thecrazyrafik@gmail.com>
2021-11-04 20:47:30 +0100

    [Spec] Test if ProviderProxy contains Providers

--- src/Provider/GitHub.php
+++ src/Provider/GitHub.php
@@ -6,4 +6,8 @@ namespace App\Provider;
 
 class GitHub implements ProviderInterface
 {
+    public function importRepositoryData(string $organization): void
+    {
+        // TODO: Implement importRepositoryData() method.
+    }
 }
