192b9ce65db086afebce2f5d96fa2b3ff1fe17d3
Rafikooo <thecrazyrafik@gmail.com>
2021-11-05 00:22:37 +0100

    [Command] Import repositories by console command - wip

---
+++ src/Command/RepositoryImporterCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Command;
+
+use App\Exception\ProviderNotExistsException;
+use App\Service\ProviderProxy;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:repository-importer',
+    description: 'Import external repositories to app',
+)]
+class RepositoryImporterCommand extends Command
+{
+    private ProviderProxy $providerProxy;
+
+    public function __construct(ProviderProxy $providerProxy)
+    {
+        $this->providerProxy = $providerProxy;
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('organization', InputArgument::REQUIRED, 'Name of organization')
+            ->addArgument('provider', InputArgument::REQUIRED, 'Name of provider')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $organization = $input->getArgument('organization');
+        $provider = $input->getArgument('provider');
+
+        try {
+            $this->providerProxy->importRepositoryData($organization, $provider);
+            $io->success('Imported successfully!');
+        } catch (ProviderNotExistsException $e) {
+            $io->error('Given provider is not supported');
+        }
+
+        return Command::SUCCESS;
+    }
+}
